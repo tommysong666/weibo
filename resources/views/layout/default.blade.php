69193ba0179933248e686a3b089c6eb30e7610e4
vagrant <vagrant@homestead>
2020-11-08 13:30:05 +0000

    静态文件浏览器缓存问题

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -6,7 +6,7 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title','weibo App')--微博官网</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
