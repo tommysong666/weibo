096b020093175597f206b3e65f53224bbc96b274
vagrant <vagrant@homestead>
2020-11-09 07:40:33 +0000

    登陆成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -28,6 +29,7 @@ class UsersController extends Controller
             'password'=>bcrypt($request->password),
             'email'=>$request->email
         ]);
+        Auth::login($user);
         session()->flash('success','注册成功，开启新旅程');
         return redirect()->route('users.show',$user);
     }
